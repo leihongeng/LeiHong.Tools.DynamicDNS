7b7f5c17dd3f2bd31389b5eb33f0e25d9b22bfc6
leihong <leihong>
2020-01-07 14:13:09 +0800

    集成apollo配置中心

--- samples/LeiHong.Tools.DynamicDNS.Sample.WebApi/Program.cs
+++ samples/LeiHong.Tools.DynamicDNS.Sample.WebApi/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using LogLevel = Com.Ctrip.Framework.Apollo.Logging.LogLevel;
 
 namespace LeiHong.Tools.DynamicDNS.Sample.WebApi
 {
@@ -20,6 +23,14 @@ namespace LeiHong.Tools.DynamicDNS.Sample.WebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, builder) =>
+                    {
+                        LogManager.UseConsoleLogging(LogLevel.Debug);
+                        builder
+                            .AddApollo("LeiHong.Tools.DynamicDNS", "https://dev-apollo.aiproject.cn/")
+                            .AddNamespace("aiproject.TencentKey")
+                            .AddNamespace("DynamicDNS");
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
